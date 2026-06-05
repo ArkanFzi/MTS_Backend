43abac79cfc4ef592673ec77eb35377c3719fcad
piannjayy <jalulajula696@gmail.com>
2026-06-05 17:35:25 +0700

    feat: implementasi riwayat edit postingan

--- Modules/User/F16_Post/Services/PostService.php
+++ Modules/User/F16_Post/Services/PostService.php
@@ -4,10 +4,14 @@ namespace Modules\User\F16_Post\Services;
 
 use Modules\User\F16_Post\Repositories\PostRepository;
 use Illuminate\Support\Facades\Auth;
+use Modules\User\F29_GamificationLeaderboard\Services\GamificationService;
 
 class PostService
 {
-    public function __construct(protected PostRepository $repo) {}
+    public function __construct(
+        protected PostRepository $repo,
+        protected GamificationService $gamification
+    ) {}
 
     public function getPosts() { return $this->repo->getAllPaginated(); }
 
@@ -20,27 +24,45 @@ class PostService
 
     public function createPost(array $data)
     {
-        $data['user_id'] = Auth::id();
+        $user = Auth::user();
+        $data['user_id'] = $user->id;
         $post = $this->repo->create($data);
         
         if (isset($data['tags'])) {
             $post->tags()->sync($data['tags']);
         }
+
+        $this->gamification->addPoints($user, 10, 'create_post', $post->id, 'Membuat postingan baru');
+
         return $post;
     }
 
     public function updatePost(string $id, array $data)
-{
-    // Update data utama
-    $post = $this->repo->update($id, $data);
+    {
+        $post = $this->repo->findById($id);
+        $oldBody = $post->body;
 
-    // Sync tags jika ada di request
-    if (isset($data['tags'])) {
-        $post->tags()->sync($data['tags']);
+        // Update data utama
+        $post = $this->repo->update($id, $data);
+
+        // Catat ke history jika body berubah
+        if (isset($data['body']) && $data['body'] !== $oldBody) {
+            $post->editHistories()->create([
+                'edited_by'   => Auth::id(),
+                'body_before' => $oldBody,
+                'body_after'  => $data['body'],
+                'reason'      => $data['edit_reason'] ?? 'Update konten',
+                'edited_at'   => now(),
+            ]);
+        }
+
+        // Sync tags jika ada di request
+        if (isset($data['tags'])) {
+            $post->tags()->sync($data['tags']);
+        }
+
+        return $post->load('tags');
     }
-
-    return $post->load('tags');
-}
 
     public function deletePost(string $id)
     {
