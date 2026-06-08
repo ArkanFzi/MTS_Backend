53601f28e4010d9773ea5c9305a4e406f89f8e5d
Fzi1 <muhammadarkanfauzi9@gmail.com>
2026-06-09 05:02:26 +0700

    fixCode: memperbaiki code seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,19 @@
 namespace Database\Seeders;
 
 use App\Models\Auth\User;
-use Illuminate\Support\Facades\Hash;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'username' => 'Test User',
-            'email' => 'test@example.com',
-            'password_hash' => Hash::make('password'),
-            
+        $this->call([
+            RoleSeeder::class,
+            UserTestSeeder::class,
+            CategorySeeder::class,
+            TagSeeder::class,
+            BadgeSeeder::class,
+            PostSeeder::class,
         ]);
     }
 }
